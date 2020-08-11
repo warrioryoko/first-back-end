f04c8ef2c05acffdd1c37cbdec1744a72bc9f3d9
warrioryoko <miangpellegri@gmail.com>
2020-08-11 15:42:03 -0700

    Refactored weather API call. Hopefully works?

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const geoData = require('./data/geo.js');
 const weatherData = require('./data/weather.js');
+const { request } = require('https');
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-function getLatLong(cityName) {
-    const city = geoData[0];
+const {
+    GEOCODE_API_KEY,
+    WEATHER_API_KEY
+} = process.env;
+
+async function getLatLong(cityName) {
+    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
+
+    const city = response.body[0];
 
     return {
         formatted_query: city.display_name,
@@ -19,32 +27,15 @@ function getLatLong(cityName) {
     };
 }
 
-function getWeather(lat,lon) {
-    // TODO: we make an api call to get the weather
-    const data = weatherData.data;
-
-    const forecastArray = data.map((weatherItem) => {
-        return {
-            forecast: weatherItem.weather.description,
-            time: new Date(weatherItem.ts * 1000)
-        };
-    });
-
-    return forecastArray;
-}
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/location', (req, res) => {
+app.get('/location', async(req, res) => {
     try {
         const locationInput = req.query.search;
     
-        const mungedData = getLatLong(locationInput);
+        const mungedData = await getLatLong(locationInput);
         res.json(mungedData);
+
     } catch(e) {
-        res.status(500).json({ error: e.message});
+        res.status(500).json({ error: e.message });
     }
 
     // res.json({
@@ -54,18 +45,43 @@ app.get('/location', (req, res) => {
     // });
 });
 
+async function getWeather(lat,lon) {
+    // TODO: we make an api call to get the weather
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat={locationLat}&lon={locationLon}&key={WEATHER_API_KEY}`);
+
+    // const data = weatherData.data;
+    const weather = response.body;
+
+    const forecastArray = weather.map((weatherItem) => {
+        return {
+            forecast: weatherItem.weather.description,
+            time: new Date(weatherItem.ts * 1000)
+        };
+    });
+
+    return forecastArray;
+}
+
 app.get('/weather', (req, res) => {
     try {
         const locationLat = req.query.latitude;
         const locationLon = req.query.longitude;
 
-        const mungedData = getWeather(locationLat, locationLon);
+        const mungedData = await getWeather(locationLat, locationLon);
         res.json(mungedData);
     } catch(e) {
         res.status(500).json({ error: e.message});
     }
 
 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
 
 // app.get('/SIMP/:id', (req, res) => {
 //     // res.send('How do I qualify for government grants? S.I.M.P. SQUIRRELS In My Pants!');
