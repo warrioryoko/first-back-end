2e25b161cea56a4919b1dc3434dc6beb3c021e5e
warrioryoko <miangpellegri@gmail.com>
2020-08-11 10:35:39 -0700

    Added dotenv, start script

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const geoData = require('./data/geo.js');
 const weatherData = require('./data/weather.js');
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT;
 
 app.use(cors());
 
